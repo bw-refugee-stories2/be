ab8b21e23ddfc6aae19c94b14f5c6db276a623a1
Brianna Workman <brianna.workman@hotmail.com>
2020-01-06 15:45:30 -0600

    deploy

--- knexfile.js
+++ knexfile.js
@@ -25,39 +25,16 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   }
 };
-
-// staging: {
-//   client: 'postgresql',
-//   connection: {
-//     database: 'my_db',
-//     user:     'username',
-//     password: 'password'
-//   },
-//   pool: {
-//     min: 2,
-//     max: 10
-//   },
-//   migrations: {
-//     tableName: 'knex_migrations'
-//   }
-// },
-
-// production: {
-//   client: 'postgresql',
-//   connection: {
-//     database: 'my_db',
-//     user:     'username',
-//     password: 'password'
-//   },
-//   pool: {
-//     min: 2,
-//     max: 10
-//   },
-//   migrations: {
-//     tableName: 'knex_migrations'
-//   }
-// }
-
-// };
