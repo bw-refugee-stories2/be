8ef63b0645a7840205ef736c56c1814b96c58956
Brianna Workman <brianna.workman@hotmail.com>
2020-01-07 11:09:29 -0600

    deploy

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const storiesRouter = require("../stories/stories-router.js");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.status(200).json({ api: "up", dbenv: process.env.DB_ENV });
+  res.status(200).json({ api: "up" });
 });
 
 server.use(helmet());
