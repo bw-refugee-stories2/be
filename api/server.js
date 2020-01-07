b6576326b098039281b167fad3bff113c3809182
Brianna Workman <brianna.workman@hotmail.com>
2020-01-07 15:00:09 -0600

    adds users endpoint

--- api/server.js
+++ api/server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const authenticate = require("../auth/authenticate-middleware.js");
 const authRouter = require("../auth/auth-router.js");
 const storiesRouter = require("../stories/stories-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
 
 server.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+  res.status(200).json({ api: "Welcome to the Refugee Stories back end! 🎉" });
 });
 
 server.use(helmet());
@@ -17,5 +19,6 @@ server.use(express.json());
 
 server.use("/api/auth", authRouter);
 server.use("/api/stories", storiesRouter);
+server.use("/api/users", authenticate, usersRouter);
 
 module.exports = server;
